948a58097c0bf31a338f037c4dcf2a3fd7835199
DeysiLopes <deysilinconl@gmail.com>
2022-06-22 00:42:21 -0300

    Create seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,13 +1,19 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(5, "Libary");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(2, "Luiz", "luiz@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
