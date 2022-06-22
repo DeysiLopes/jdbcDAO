50fd176b0476d513ce866b810a371cba2b3d1fa7
DeysiLopes <deysilinconl@gmail.com>
2022-06-22 02:04:08 -0300

    Implements reuse instantiation

--- src/app/Program.java
+++ src/app/Program.java
@@ -10,6 +10,7 @@ public class Program {
 		
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== Test 1: findById seller ===");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
