3402ba01b8543135fb7a9da29b698a761b83f769
DeysiLopes <deysilinconl@gmail.com>
2022-06-22 00:29:29 -0300

    Create department entity class

---
+++ src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(5, "Libary");
+		System.out.println(obj);
+
+	}
+
+}
